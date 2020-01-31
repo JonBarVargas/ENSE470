7c7ecd260b302bba88cb1ab8d35449fd3f255fc0
Jonathan Vargas <jonathanbvargas@hotmail.com>
2020-01-31 16:04:55 -0600

    Finished Hangman

--- Lab00HangMan/src/HangmanBoard.java
+++ Lab00HangMan/src/HangmanBoard.java
@@ -1,9 +1,17 @@
+/**
+ * HangmanBoard.java
+ * 
+ *This class handles the variables and the functions of a hangman game
+ *
+ * 
+ * @author Jonathan Vargas
+ * @version 1.0.0 Jan 31, 2020
+ */
 public class HangmanBoard {
 	private String secretWord = "";
 	private String boardWord ="";
 	private int numOfTries;
 	private String triedChars = "";
-
 
 	public HangmanBoard() {
 		numOfTries = 0;
@@ -12,50 +20,95 @@ public class HangmanBoard {
 		boardWord ="";
 	}
 	
-	//done
 	public HangmanBoard(int maxGuesses, String userWord) {
 		numOfTries = maxGuesses;
 		secretWord = userWord.toLowerCase();
 		for (int i = 0; i < secretWord.length(); i++)
 			boardWord += "*";
 	}
+
+	//checks if the user has re enters on of their guesses or if the char is in the secret word
+	public boolean validateGuess(char guess)
+	{
+		String guessString = String.valueOf(guess); 
+		guessString.toLowerCase();
+		boolean correctGuess = secretWord.contains(guessString);
+
+		if(triedChars.contains(guessString)) {
+			numOfTries--;
+			return false;
+		}
+		else if (!correctGuess)
+		{
+			numOfTries--; 
+		}
+		triedChars += guessString + ", ";
+		this.updateBoard(correctGuess, guess);
+		return correctGuess;
+	}
 	
-	//done
+	public boolean validateGuess(String guess)
+	{
+		boolean correctGuess = secretWord.equalsIgnoreCase(guess);
+		if (!correctGuess)
+		{
+			numOfTries--; 
+		}
+		this.updateBoard(correctGuess, guess);
+		return correctGuess;
+	}
+	
+	private void updateBoard(boolean guessState, char userChar)
+	{
+		
+		if (guessState)
+		{
+			//this loops through the string and find the chars that match in secretWord and replaces
+			//the astrik in the current position as the user entered char.
+			for (int i = 0; i < secretWord.length(); i++)
+			{
+				if ( secretWord.charAt(i) == userChar)
+				{
+					boardWord = boardWord.substring(0,i)+ userChar + boardWord.substring(i+1);
+				}
+			}
+		}
+		
+		//this is ambiguous code
+		//in theory after the astriks are removed by the algorithm above boardword == secretWord
+		//It would compile the strings as 2 different strings
+		//to remedy this i search boardWord if it stills contains astriks then assign boardWord as
+		// secret word to communicate with my other functions
+		if (!(boardWord.contains("*")))
+		{
+			boardWord = secretWord;
+		}
+	}
+	
+	private void updateBoard(boolean guessState, String guess) 
+	{
+		if (guessState)
+		{
+			boardWord = secretWord;
+		}
+	}
+	
+	
 	public String getSecretWord()
 	{
 	return secretWord;
 	}
-	
-//	public String getDisplayBoardWord() {
-	//return boardWord;
-	//}
-	
-	public void enterLetter(char guess) {
-		char ch1 = Character.toLowerCase(guess);
-		this.validateGuess(ch1);
-	}
-	private void validateGuess(char guess)
+	public int getNumOfTries()
 	{
-		boolean correctGuess = false;
-		String guessString = String.valueOf(guess); 
-		correctGuess = secretWord.contains(guessString);
-		System.out.println(secretWord.contains(guessString));
+	return numOfTries;	
 	}
-	public void validateGuess(String guess)
+	public String getBoardWord() 
 	{
-		
-		//guess.toLowerCase();
-		System.out.println(secretWord.equalsIgnoreCase(secretWord));
+	return boardWord;
 	}
-	private void updateBoard(boolean guessState, char userChar)
+	public String getTriedChars() 
 	{
-		for (int i = 0; i < secretWord.length(); i++)
-		{
-			if ( secretWord.charAt(i) == userChar)
-			{
-				//boardWord.setCharAt
-			}
-		}
+	return triedChars;
 	}
 	
 
--- Lab00HangMan/src/Lab00jbv071.java
+++ Lab00HangMan/src/Lab00jbv071.java
@@ -1,19 +1,117 @@
+/**
+ * Lab00jbv071.java
+ * 
+ *This is a build of a game of hangman using the
+ *built HangmanBoard class
+ *
+ *player 1 gives the program a word that player 2 must 
+ *guess through guessing letters or the word
+ * 
+ * @author Jonathan Vargas
+ * @version 1.0.0 Jan 31, 2020
+ */
+
+import java.util.Scanner;
 
 public class Lab00jbv071 {
 
-	public static void main(String[] args) {
-		HangmanBoard boi = new HangmanBoard(5, "yeeyeet");
-		String omg ="yeet";
-		String test ="YEEyeet";
-		
-		//boi.displayBoard();
-		//boi.enterLetter('Y');
-		boi.validateGuess(test);
+
 	
-	//	System.out.println(omg.equalsIgnoreCase(test));
-		//String myName = "domanokz";
-		//String newName = myName.substring(0,4)+'x'+myName.substring(5);
-		//System.out.println(newName);
+	public static void main(String[] args) {
+		String user1Word = "";
+		int numOfLives = 5;
+		int userChoice;
+		
+		boolean winGame = false;
+		boolean validUserChoice = false;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to hangman!");
+		System.out.println("Player 1 please enter your secret word");
+		user1Word = input.nextLine();
+		
+		for (int i = 0; i<= numOfLives; i++)
+		{
+			System.out.println("********************");
+		}
+	    HangmanBoard newGame = new HangmanBoard(numOfLives, user1Word);
+	    
+	    //round structure
+	    // 1. displays the players status in the game
+	    // 2. displays the choices the user can enter
+	    // 3. processes the players status
+	    // 4. checks if the user is out of tries or guessed the word
+		while ((newGame.getNumOfTries() != 0) && !winGame)
+		{
+			while(!validUserChoice)
+			{
+				System.out.println("The word is: " + newGame.getBoardWord());
+				System.out.println("You have " + newGame.getNumOfTries() + " tries remaining");
+				System.out.println("You have tried these characters: " + newGame.getTriedChars());
+				System.out.println("Player 2, you have two choices:");
+				System.out.println("Enter 1 to guess a letter.");
+				System.out.println("Enter 2 to guess the word ");
+			
+				userChoice = input.nextInt();
+				
+				switch(userChoice)
+				{
+				case 1: validUserChoice = true;
+						enterLetter(newGame); 
+						break;
+				case 2: validUserChoice = true;
+						enterWord(newGame); break;
+				default: System.out.println("This is an incorrect input please try again");
+				}
+			}
+			validUserChoice = false;
+			if (newGame.getBoardWord() == newGame.getSecretWord()) {
+				winGame = true;
+				System.out.print("you got it! the word was: " + user1Word);
+			}
+			if (newGame.getNumOfTries() == 0) {
+				System.out.println("sorry it you ran out of lives");
+			}
+			
+		}
+		
 	}
+	
+	/**
+	 * This function takes in the users word and checks if the secret word matches
+	 */
+	static void enterWord(HangmanBoard game)
+	{
+		Scanner input = new Scanner(System.in);
+		String user2GuessStr = "";
+		user2GuessStr = input.nextLine();
+		game.validateGuess(user2GuessStr);
+		boolean valid = game.validateGuess(user2GuessStr);
+		if(!valid)
+		{
+			System.out.println("Sorry the secret word is not " + user2GuessStr);
+		}
+	}
+	
+	/**
+	 * This function takes in the users letter and checks if it matches with any of the letters 
+	 * in the secret word
+	 */
+	static void enterLetter(HangmanBoard game)
+	{
+		Scanner input = new Scanner(System.in);
+		char user2GuessCha;
+		System.out.println("Please enter a letter:");
+		user2GuessCha = input.next().charAt(0);
+		boolean valid = game.validateGuess(user2GuessCha);
+		if (valid) {
+			System.out.println("There is a " + user2GuessCha);
+		}
+		else
+		{
+			System.out.println("Oops try again");
+		}
+	}
+	
+	
 
 }
