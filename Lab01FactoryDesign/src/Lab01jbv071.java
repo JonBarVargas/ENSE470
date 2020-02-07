0b106f1dc04467b43d9ead007208fce9c18a6610
Jonathan Vargas <jonathanbvargas@hotmail.com>
2020-02-07 12:47:09 -0600

    Implemented Factory pattern
    Made the tanks able to be made during runtime

--- Lab01FactoryDesign/src/Lab01jbv071.java
+++ Lab01FactoryDesign/src/Lab01jbv071.java
@@ -1,13 +1,66 @@
+/*
+ * Lab01jbv071.java
+ * 
+ * This is code to test 5 different tanks
+ * 
+ * Using the factory method tanks are being made during 
+ * run time and we test the tanks durability by firing
+ * at it
+ * 
+ * @author Jonathan Vargas
+ * @version 1.0.0 Feb 07, 2020
+ */
+import java.util.Scanner;
 
 public class Lab01jbv071 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TankFactory yeetFactory = new TankFactory();
 		
+		final int TESTROUNDS = 5;
+		Scanner userInput = new Scanner(System.in);
+		String tankType = "";
+		TankFactory testFactory = new TankFactory();
 		
-		Tank tester = null;
-		tester = yeetFactory.makeNewTank("yeet");
-		System.out.println(tester.getCountry());
+		for(int i = 1; i <= TESTROUNDS; i++) {
+			Tank tester = null;
+			System.out.println("Round " + i);
+			System.out.println("Please enter what kind of tank you would like to test: ");
+			System.out.println("Cunningham, Crusader, Nameless, Yazi, Renault");
+			
+			if (userInput.hasNextLine()) {
+				tankType = userInput.nextLine();
+				tester = testFactory.makeNewTank(tankType);
+				if (tester != null) {
+					testTheTank(tester, i);
+				}
+			}
+		}
+	}
+	
+	//This function displays the tanks info and shoots the tank by an arbriturary 
+	//amount of shots and rounds
+	public static void testTheTank(Tank userTank, int modifier) {
+		//The modifiers are meant to increase the difficulty of the 
+		//test based on what round it is
+		final int TESTSHOT = 70*modifier;
+		final int TESTPENETRATION = 55*modifier;
+		final int SHOOTROUNDS = 3+modifier;
+		
+		System.out.println("Tank name: " + userTank.getName());
+		System.out.println("Country: " + userTank.getCountry());
+		System.out.println("Max HP: " + userTank.getHitPoints());
+		System.out.println("Armor: " + userTank.getArmor());
+		System.out.println("Shell Damage: " + userTank.getShellDamage());
+		
+		System.out.println("");
+		System.out.println("Test shot damage: " + TESTSHOT);
+		System.out.println("Test shot penetration: " + TESTPENETRATION);
+		System.out.println("");
+		
+		for(int i = 1; i <= SHOOTROUNDS; i++) {
+		System.out.println("Firing missle "+ i + "...");
+		userTank.receiveHit(TESTSHOT, TESTPENETRATION);
+		System.out.println("Tank HP: " + userTank.getCurrentHealth());
+		}
 	}
 }
--- Lab01FactoryDesign/src/TankFactory.java
+++ Lab01FactoryDesign/src/TankFactory.java
@@ -1,10 +1,24 @@
 
 public class TankFactory {
 	
-	public Tank makeNewTank(String newShipTypes) {
+	public Tank makeNewTank(String tankTypes) {
 		Tank testTank = null;
-		if (newShipTypes.contentEquals("yeet")) {
+		tankTypes.toLowerCase();
+		if (tankTypes.equals("cunningham")) {
 			testTank = new T1Cunningham();
+		}else
+		if(tankTypes.equals("crusader")) {
+			testTank = new Crusader();
+		}else
+		if(tankTypes.equals("nameless")) {
+			testTank = new TheNameless();
+		}else
+		if(tankTypes.equals("yazi")) {
+			testTank = new Yazi();
+		}
+		else
+		if(tankTypes.equals("renault")) {
+			testTank = new Renault();
 		}
 		return testTank;
 	}
